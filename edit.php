ab45fa6979a113a112db97faf3906be3d41cc27d
Dwi Wijonarko <d.wijonarko@gmail.com>
2014-06-23 08:40:58 +0700

    menambahkan jquery dan bootstrap.js pada halaman edit

--- edit.php
+++ edit.php
@@ -104,6 +104,8 @@ if (isset($_SESSION['login'])&& $_SESSION['login']==true) {
       </div>
       </div>
     </row>
+<script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
 <?php
