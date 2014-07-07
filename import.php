1ba6fa29ae2fba97a31ffae6e54db495a8e72e88
dwijonarko <d.wijonarko@gmail.com>
2014-07-07 14:11:56 +0700

    add form import from excel

--- import.php
+++ import.php
@@ -1,25 +1,69 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Import Excel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div class="container">
+    <h1>Import from excel</h1>
+    <form class="form-inline" role="form" action="" method="POST" enctype="multipart/form-data">
+      <div class="form-group">
+        <label class="sr-only" for="exampleInputEmail2">Upload Excel File (*.xls, *.xlsx)</label>
+        <input type="file" class="form-control" id="uploadedFile" name="uploadedFile">
+      </div>
+      <button type="submit" class="btn btn-default" name="import">Import</button>
+    </form>
 
-define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+    <?php
+      if (isset($_POST['import'])) {
+        try {
+        $pdo = new PDO('mysql:host=localhost;dbname=pemrograman_web','root', 'root');
+        $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+        $sql ="INSERT INTO books (title,author,description,on_sale,cover) VALUES (:title,:author,:description,:on_sale,:cover)";
+        $data = $_FILES['uploadedFile']['tmp_name'];
+        $q = $pdo->prepare($sql);
+        require_once dirname(__FILE__) . '/Classes/PHPExcel/IOFactory.php';
 
-date_default_timezone_set('Asia/Jakarta');
+        $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+        $objPHPExcel = $objReader->load($data);
+        echo "<p> Last imported data</p>";
+        echo "<table class='table'>";
+        foreach ($objPHPExcel->getActiveSheet()->getRowIterator(3) as $row){
+          echo "<tr>";
+          $val=array();
+          $cellIterator = $row->getCellIterator();
+          $cellIterator->setIterateOnlyExistingCells(false);
+          
+          foreach ($cellIterator as $index => $cell) {
+              $val[]=$cell->getValue();
+          }
+          if($q->execute(array(':author'=>$val[1],':title'=>$val[2],':description'=>$val[3],':on_sale'=>$val[4],':cover'=>$val[5]))){
+            echo "<td>$val[0]</td><td>$val[1]</td><td>$val[2]</td><td>$val[3]</td><td>$val[4]</td><td>$val[5]</td>";
+          }
+          echo "</tr>";
+        }
+        echo "</table>";
+      }catch(Exception $error) {
+        echo $error->getMessage();
+      }
+    }
+    ?>
+  </div>
 
-/** PHPExcel_IOFactory */
-require_once dirname(__FILE__) . '/Classes/PHPExcel/IOFactory.php';
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
 
-$objReader = PHPExcel_IOFactory::createReader('Excel2007');
-$objPHPExcel = $objReader->load("books.xlsx");
-
-
-foreach ($objPHPExcel->getActiveSheet()->getRowIterator(2) as $row){
-	$cellIterator = $row->getCellIterator();
-	$cellIterator->setIterateOnlyExistingCells(false);
-	foreach ($cellIterator as $cell) {
-    	echo $cell->getValue()." ";
-	}
-	echo "<br>";
-}
--- import_excel_reader.php
+++ import_excel_reader.php
@@ -1,14 +1,53 @@
-<?php 
-require 'excel_reader2.php';
-$data = new Spreadsheet_Excel_Reader("books.xls");
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Import Excel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-$baris = $data->rowcount($sheet_index=0);
-for ($i=2; $i<=$baris; $i++) {
-  $id = $data->val($i,1);
-  $title = $data->val($i,2);
-  $author = $data->val($i,3);
-  $description = $data->val($i,4);
-  $on_sale = $data->val($i,5);
-  $cover = $data->val($i,6);
-  echo "$id $title $author $description $on_sale $cover <br>";
-}
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div class="container">
+    <h1>Import from excel</h1>
+    <form class="form-inline" role="form" action="" method="POST" enctype="multipart/form-data">
+      <div class="form-group">
+        <label class="sr-only" for="exampleInputEmail2">Upload Excel File (*.xls, *.xlsx)</label>
+        <input type="file" class="form-control" id="uploadedFile" name="uploadedFile">
+      </div>
+      <button type="submit" class="btn btn-default" name="import">Import</button>
+    </form>
+    <?php 
+     if (isset($_POST['import'])) {
+        $data = $_FILES['uploadedFile']['tmp_name'];
+        require 'excel_reader2.php';
+        $data = new Spreadsheet_Excel_Reader($data);
+        echo "<table class='table'>";
+        $baris = $data->rowcount($sheet_index=0);
+        for ($i=2; $i<=$baris; $i++) {
+          $id = $data->val($i,1);
+          $title = $data->val($i,2);
+          $author = $data->val($i,3);
+          $description = $data->val($i,4);
+          $on_sale = $data->val($i,5);
+          $cover = $data->val($i,6);
+          echo "<tr>";
+          echo "<td>$id</td><td>$title</td><td>$author</td><td>$description</td><td>$on_sale</td><td>$cover</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+    }?>
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
